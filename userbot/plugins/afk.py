bcd0b1e9d009a9ac18d4af8beca6f0222ddfacdf
shubhanshdj <71318599+shubhanshdj@users.noreply.github.com>
2020-10-25 17:25:22 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -37,7 +37,7 @@ async def set_not_afk(event):
     if ".afk" not in current_message and "yes" in USER_AFK:  # pylint:disable=E0602
         shite = await borg.send_message(
             event.chat_id,
-            "__Back alive!__\n**No Longer afk.**\n `Was afk for:``"
+            "__I am back alive Only legend can do this!__\n**kya karu Time bahut hai afk.**\n `AFk Timing :``"
             + total_afk_time
             + "`",
         )
@@ -45,7 +45,7 @@ async def set_not_afk(event):
             await borg.send_message(  # pylint:disable=E0602
                 Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
                 "#AFKFALSE \nSet AFK mode to False\n"
-                + "__Back alive!__\n**No Longer afk.**\n `Was afk for:``"
+                + "__I am back alive Only legend can do this!__\n**kya karu bahut time hai afk.**\n `AFk timing:``"
                 + total_afk_time
                 + "`",
             )
@@ -117,10 +117,10 @@ async def on_afk(event):
         #           afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
         message_to_reply = (
-            f"__My Master Has Been In afk since__ `{total_afk_time}`\nWhere He Is: I don't know buddy..he is a busy person "
-            + f"\n\n__I can't guarantee you that when he will come..__\n**Ohh!!He left a REASON** __BTW__🧐: {reason}"
+            f"__Mera bhagwan Abhi Afk pe hai be lawde__ `{total_afk_time}`\nWhere He Is: I don't know buddy..he is a busy person "
+            + f"\n\n__I can't tell You ki Wo Kab wapass aayenge lekin..__\n**Ohh!!He left a REASON** __BTW__🧐: {reason}"
             if reason
-            else f"**Heyy!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? Soon __Whenever I feel like coming back__**(o(^â–½^)o)**  "
+            else f"**Heyy!**\n__I am Not Available Now jo karna wo Karo itne time se {total_afk_time} I guess.__\n\nWhen will I be back? Soon __Whenever I feel like coming back__**(o(^â–½^)o)**  "
         )
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
@@ -155,7 +155,7 @@ async def _(event):
         USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
             await borg.send_message(
-                event.chat_id, f"**I shall be Going afk!** __because ~ {reason}__"
+                event.chat_id, f"**I Am Going To A.F.K plz Don't Disturb!** __kyu ki ~ {reason}__"
             )
         else:
             await borg.send_message(event.chat_id, f"**I am Going afk!**")
